8117b2cebd61bc3e396544df3022fec2af80b0ca
Talgat Amanbayev <amanbayev@gmail.com>
2020-07-30 00:45:16 +0600

    Homework 2 step 1

--- src/Cats/RandomCatImage.js
+++ src/Cats/RandomCatImage.js
@@ -1,5 +1,5 @@
-import React from "react";
-import styled from "styled-components";
+import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 
 const MainContainer = styled.div`
   display: flex;
@@ -13,23 +13,40 @@ const Image = styled.img`
 // https://docs.thecatapi.com/
 
 function RandomCatImage(props) {
-  const [catImageUrl, setCatImageUrl] = React.useState(null);
+  const [catImageUrl, setCatImageUrl] = useState(null);
+  const [timeLeft, setTimeLeft] = useState(30);
 
-  React.useEffect(() => {
-    fetch("https://api.thecatapi.com/v1/images/search")
+  // variable to trigger fetch effect hook. ignore the value, just switching T and F
+  const [refetch, setRefetch] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setTimeLeft(timeLeft - 1);
+      if (timeLeft === 0) {
+        setTimeLeft(30);
+        setRefetch(!refetch);
+        setCatImageUrl(null);
+      }
+    }, 1000);
+    return () => clearTimeout(timer);
+  });
+
+  useEffect(() => {
+    fetch('https://api.thecatapi.com/v1/images/search')
       .then((response) => {
         return response.json();
       })
       .then((data) => {
         setCatImageUrl(data[0].url);
       });
-  }, []);
+  }, [refetch]);
 
   if (catImageUrl == null) return <div> Loading </div>;
 
   return (
     <MainContainer>
       <Image src={catImageUrl} />
+      <p>New cat image will appear in {timeLeft} seconds.</p>
     </MainContainer>
   );
 }
